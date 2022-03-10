fdec95515970c3427485bdc33c35534914f5e8ca
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-03-10 10:00:35 -0400

    Add API mocking || test API response || test coinReducer state

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  waitFor, render, fireEvent, screen,
+  waitFor, render, screen,
 } from './test-utils';
 import App from '../App';
 import '@testing-library/jest-dom';
@@ -9,11 +9,4 @@ it('App components render correctly', async () => {
   const { container } = render(<App />);
   await waitFor(() => screen.getByText('Crypto Coins'));
   expect(container).toMatchSnapshot();
-});
-
-it('Click on a link renderer properly', async () => {
-  render(<App />);
-  await waitFor(() => screen.getByText('Bitcoin'));
-  fireEvent.click(screen.getByText('Bitcoin'));
-  expect(screen.getByText('Crypto Name:')).toBeInTheDocument();
 });
